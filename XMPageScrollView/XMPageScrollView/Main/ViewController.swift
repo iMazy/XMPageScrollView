54adf82650be9fff76c23b09b7aa691395cec449
Mazy <mazy_ios@163.com>
2017-07-27 13:27:31 +0800

    feat： 完成演示

--- XMPageScrollView/XMPageScrollView/Main/ViewController.swift
+++ XMPageScrollView/XMPageScrollView/Main/ViewController.swift
@@ -12,12 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let titleArray: [String] = ["推荐","热点","北京"]//,"视频","社会","图片","娱乐","科技","汽车"]
+        var childVC: [UIViewController] = [UIViewController]()
+        
+        for _ in 0..<titleArray.count {
+            let vc = UIViewController()
+            vc.view.backgroundColor = UIColor.randomColor().withAlphaComponent(0.3)
+            childVC.append(vc)
+        }
+        
+        let property: TitleViewProperty = TitleViewProperty()
+        
+        // property.isScrollEnable = true
+        property.contentOffsetAnimated = false
+        
+        property.isNeedShadowInBottom = true
+        
+        let containerView = XMPageScrollView(frame: CGRect(x: 0, y: 64, width: view.bounds.width, height: view.bounds.height-64), titles: titleArray, childVC: childVC, parentVC: self, titleProperty: property)
+        
+        view.addSubview(containerView)
     }
 
 
